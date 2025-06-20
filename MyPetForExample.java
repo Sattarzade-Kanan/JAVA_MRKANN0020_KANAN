0d9eb86c4aad57b933299efbdb2f743a6b9b9a18
Sattarzade-Kanan <chinkan1980@gmail.com>
2025-06-20 10:12:12 +0400

    Kenan-Sattarzade
    Здраствуйте учитель вот мое дз!

---
+++ MyPetForExample.java
@@ -0,0 +1,20 @@
+package Homwerk;
+
+import JAV.Person;
+
+public class MyPetForExample {
+    public static void main(String[] args) {
+        MyPet Kat = new MyPet();
+
+
+        Kat.name = "Murka";
+        Kat.type = "Bulbak";
+        Kat.price = 134;
+        Kat.ageRestriction = 12 ;
+        Kat.customerAge = 13;
+Kat.MyMoney = 99;
+
+
+        Kat.mypet_info();
+    }
+}
