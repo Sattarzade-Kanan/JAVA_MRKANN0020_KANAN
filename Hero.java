f84b17932fdaaa41e6cf47966f4be0e67d80e1ba
Sattarzade-Kanan <chinkan1980@gmail.com>
2025-06-24 13:46:41 +0400

    урок кянанан

---
+++ Hero.java
@@ -0,0 +1,34 @@
+package Homwerk;
+
+public class Hero {
+    String name;
+    int xp;
+
+
+    class Main {
+        public static void main(String[] args) {
+            Voin lea = new Voin();
+            lea.leavedAttack();
+            xp = 100;
+            name = bublik;
+        }
+        class Main {
+            public static void main(String[] args)
+            }
+        }
+    }
+    void attack() {
+        System.out.println("Герой атакует!");
+    }
+    void leavedAttack() {
+        System.out.println("Воин делает мощную атаку!");
+    }
+
+    void mag(){
+        System.out.println("Маг колдует закленание!");
+    }
+
+    void lucnik(){
+        System.out.println("Лучник стреляет из лука!");
+    }
+}
