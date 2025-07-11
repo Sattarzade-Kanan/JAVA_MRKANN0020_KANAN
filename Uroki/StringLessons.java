020690e72ffd002483c061f88170a6796a618a1b
Sattarzade-Kanan <chinkan1980@gmail.com>
2025-07-11 13:56:50 +0400

    String Kenan

---
+++ Uroki/StringLessons.java
@@ -0,0 +1,13 @@
+package JAV;
+
+public class StringLessons {
+    public static void main(String[] args) {
+        String str = "Это новый урок внимательно";
+String str1 = "Скоро закончю ";
+
+        System.out.println(str.length());
+        System.out.println(str.substring(0));
+        System.out.println(str.substring(22,26));
+        System.out.println(str.equals(str1));
+    }
+}
