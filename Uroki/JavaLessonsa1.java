1a87995fb8600c453bbdee1251ae126ad6d37b31
Sattarzade-Kanan <chinkan1980@gmail.com>
2025-07-15 12:40:39 +0400

    kanan

---
+++ Uroki/JavaLessonsa1.java
@@ -0,0 +1,11 @@
+package JAV;
+
+public class JavaLessonsa1 {
+    public static void main(String[] args) {
+        String str = "Java eto lucsiy yazik";
+        String str1  = "Prosto Java";
+
+        System.out.println(str.substring(0 , 4));
+        System.out.println(str.equals(str1));
+    }
+}
