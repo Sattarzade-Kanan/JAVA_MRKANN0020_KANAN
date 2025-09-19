f17706570780c98e5ca7feddd23fac03c11b0da9
Sattarzade-Kanan <chinkan1980@gmail.com>
2025-09-19 09:26:15 +0400

    Dz Kenana

---
+++ GitHubDesktobHomwerks/MainPraibroovanie.java
@@ -0,0 +1,40 @@
+package GitHubDesktobHomwerks;
+
+import java.util.Scanner;
+
+public class MainPraibroovanie {
+    public static void main(String[] args) {
+        int akula = 200;
+        byte b = (byte)akula;
+        System.out.println(b);
+
+
+        double sigma = 12.75;
+        int c = (int)sigma;
+        System.out.println(c);
+
+//        Уровень 2!
+
+        byte x= 50;
+        short y = 60;
+        int plius = x+y;
+
+        byte sigma1 = (byte)plius;
+        System.out.println(sigma1);
+
+        long l = 100000;
+        float tat = (float)l;
+        System.out.println("Creating///"+tat);
+
+
+        double ttat = (double)l;
+        System.out.println("Creating 1///" + ttat);
+
+        Scanner scanner = new Scanner(System.in);
+
+        double price = 67.45;
+
+        byte sigma100 = (byte)price;
+        System.out.println(sigma100);
+    }
+}
